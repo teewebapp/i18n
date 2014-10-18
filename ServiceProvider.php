be362922de74bebf2f877642ee5f3a6f8fb009b4
Anderson Danilo <contato@andersondanilo.com>
2014-10-17 22:35:16 -0300

    alteração

--- ServiceProvider.php
+++ ServiceProvider.php
@@ -9,7 +9,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider {
 
     public function register()
     {
-        require_once app_path().'/modules/i18n/macros/form.php';
+        require_once app_path().'/macros/form.php';
         // registra os widgets
         Widget::register(
             'languageMenu',
